23f1ab8d1ffe053f5dcf834908480b427334a80b
Iman <josefhachiiman@gmail.com>
2023-11-08 10:35:45 +0200

    function App modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,51 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import image from './assets/panda.png';
 
+//user or name or message etc.
+function Greeting({ name }) {
+  if (name === 'Iman') {
+    return <p>Welcome {name}</p>
+  }
+  return <p>Please log in.</p>
+}
+console.log(Greeting);
+
 function App() {
-  const [persons, setPersons] = useState([{ id: 0, name: 'Jane Doe', title: 'CEO', age: 21 },
-  { id: 1, name: 'Ann', title: 'BOSS', age: 39 },
-  { id: 2, name: 'Randy', title: 'Employee', age: 28 },
+  const [persons, setPersons] = useState([
+    { id: 0, name: 'Jane', title: 'CEO', age: 21 },
+    { id: 1, name: 'Ann', title: 'BOSS', age: 18 },
+    { id: 2, name: 'Randy', title: 'Employee', age: 28 },
   ]);
 
   /*const eventHandler = (name) => {
     console.log(name);
   }*/
+
   const removeHandler = (id) => {
-    const updatedArray = persons.filter(person => person.id !== id)
-    setPersons(updatedArray)
-  }
+    const updatedArray = persons.filter((person) => person.id !== id);
+    setPersons(updatedArray);
+  };
+
   return (
     <>
-      <header logo="IMAN HAJI" />
-      <h1>This is my application.</h1>
+      <Header logo="Iman Haji" />
       <main>
+        <h2>This is my App.</h2>
         <div className="cards">
           {persons.map((person) =>
           (<Card
             key={person.id}
-            {...person.id}
+            {...person}
             click={() => removeHandler(person.id)}
           />
           ))}
         </div>
-        <button onClick={removeHandler}>Click me</button>
-      </main >
-      <footer copyright="hi" />
+      </main>
+      <Footer copyright="Copyright smth" />
     </>
   );
 }
+
 //arrayFilter
 //()=>  binds the data of person name 
 //make a state which has array with three people
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,23 @@
 const Card = ({ name, age, click }) => {
   return (
     <div className="card">
+      <div style={age > 30 ? { background: "grey" } : { background: "pink" }} className="Card">
+      </div>
+
+
+
       <h1>name: {name}</h1>
-      <p>age: {age}</p>
+      <p className={age > 30 ? "pink-age" : "red-age"}>age: {age}</p>
       <button onClick={click}>.....</button>
     </div>
   );
 };
 
 export default Card;
+
+/*function AgeOfUser({ age }) {
+  return ((age === 80) ? <p>Your age is {age}</p> : <p>Please type out your age.</p>)
+}
+<AgeOfUser age={age} />;
+
+ */
